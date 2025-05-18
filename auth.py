d33f7f66322532f92230680e476b8c8a899ac99d
Diionii <Ddion230@gmail.com>
2025-05-18 23:50:27 +0200

    verifikimi me email dhe javascript per 2FA

--- auth.py
+++ auth.py
@@ -9,6 +9,8 @@ from email.message import EmailMessage
 
 app = Flask(__name__)
 
+DATABASE_FILE = 'users.txt'
+users = []
 def save_users():
     with open(DATABASE_FILE, 'w') as f:
         json.dump(users, f, indent=4)
@@ -26,7 +28,7 @@ if os.path.exists(DATABASE_FILE):
         except json.JSONDecodeError:
             users = []
 
-
+# G1
 @app.route('/')
 @app.route('/user_access')
 def user_access():
@@ -68,6 +70,7 @@ def login():
         return jsonify({"message": "Login successful"})
     return jsonify({"message": "Invalid credentials"}), 401
 
+# G1
 @app.route('/setup_totp', methods=['POST'])
 def setup_totp():
     email = request.form['email']
@@ -78,13 +81,40 @@ def setup_totp():
         return jsonify({"qr_url": qr_url})
     return jsonify({"message": "User not found"}), 404
 
-
 @app.route('/send_email_code', methods=['POST'])
 def send_email_code():
     email = request.form['email']
     user = find_user(email)
     if not user:
         return jsonify({"message": "User not found"}), 404
+
+    verification_code = str(random.randint(100000, 999999))
+
+    if send_verification_code(email, verification_code):
+        user['pending_code'] = verification_code
+        return jsonify({"message": "Code sent"})
+    else:
+        return jsonify({"message": "Failed to send email code"}), 500
+
+def send_verification_code(to_email, code):
+    EMAIL_ADDRESS = "dion.haradinaj@student.uni-pr.edu"
+    EMAIL_PASSWORD = "geus ezmz ihmd bime"
+
+    msg = EmailMessage()
+    msg['Subject'] = 'Kodi juaj i verifikimit'
+    msg['From'] = EMAIL_ADDRESS
+    msg['To'] = to_email
+    msg.set_content(f'Kodi juaj i verifikimit është: {code}')
+
+    try:
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+            smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+            smtp.send_message(msg)
+        print("Email u dërgua me sukses!")
+        return True
+    except Exception as e:
+        print(f'Dështoi dërgimi i emailit: {e}')
+        return False
 
 @app.route('/verify_2fa', methods=['POST'])
 def verify_2fa():
@@ -112,3 +142,10 @@ def verify_2fa():
             return jsonify({"message": "Invalid email verification code"}), 400
 
     return jsonify({"message": "Invalid 2FA method"}), 400
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
